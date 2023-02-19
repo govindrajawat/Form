77277bc502a5e84428ac8a3f6a2076b3dfc85744
Govindrajawat <govindrajawat260398@gmail.com>
2023-02-19 23:19:24 +0530

    1.0

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-const signInBtn = document.getElementById("signIn");
-const signUpBtn = document.getElementById("signUp");
-const fistForm = document.getElementById("form1");
-const secondForm = document.getElementById("form2");
-const container = document.querySelector(".container");
-
-signInBtn.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
-
-signUpBtn.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
-
-fistForm.addEventListener("submit", e => e.preventDefault());
-secondForm.addEventListener("submit", e => e.preventDefault());
-
-const toggleButton = document.querySelector('.dark-button');
+let toggleButton = document.querySelector('.dark-button');
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
+});
+
+let signInBtn = document.getElementById("signIn");
+let signUpBtn = document.getElementById("signUp");
+let app = document.querySelector(".app");
+signInBtn.addEventListener("click", () => {
+  app.classList.remove("swap");
+});
+signUpBtn.addEventListener("click", () => {
+  app.classList.add("swap");
 });
